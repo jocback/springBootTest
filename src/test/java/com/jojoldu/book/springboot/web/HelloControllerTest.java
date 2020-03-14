406530d469059537d2f4bc9df5cb20ee89c3f9b5
윤재훈 <jocback@gmail.com>
2020-03-14 18:50:26 +0900

    gitignore

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -17,7 +17,7 @@ public class HelloControllerTest {
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception {
+    public void hello_return() throws Exception {
         String hello = "hello";
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
@@ -25,7 +25,7 @@ public class HelloControllerTest {
     }
 
     @Test
-    public void helloDto가_리턴된다() throws Exception {
+    public void helloDto_return() throws Exception {
         String name = "hello";
         int amount = 1000;
 
